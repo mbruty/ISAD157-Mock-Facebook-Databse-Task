e628a08da60edaa1e24a7c00d421eaa686e70a48
mbruty <48282784+BigBoiBruty@users.noreply.github.com>
2020-05-03 23:00:40 +0100

    Moved in to sub folder

---
+++ Forms/Message Forms/MessageForm.cs
@@ -0,0 +1,115 @@
+﻿using FacebookUI.Forms;
+using MySqlX.XDevAPI.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookUI
+{
+    public partial class MessageForm : Form
+    {
+        protected int userID, viewID;
+        private string viewName;
+
+        public MessageForm()
+        {
+            InitializeComponent();
+        }
+        public MessageForm(int userID, int viewID, string viewName)
+        {
+            InitializeComponent();
+            this.userID = userID;
+            this.viewID = viewID;
+            this.viewName = viewName;
+            this.toTxtBox.Text = viewName;
+        }
+
+        private void populateData()
+        {
+            Task.Run(async () =>
+            {
+                return await DataBase.getMessages(this.userID, this.viewID);
+            }).ContinueWith((result) => 
+            { 
+                Util.SetControlPropertyThreadSafe(messageDGV, "DataSource", result.Result);
+            });
+        }
+
+        private void messageDGV_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            messageDGV.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            messageDGV.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+        }
+
+        protected virtual void sendBtn_Click(object sender, EventArgs e)
+        {
+            string text = msgTxtBox.Text;
+            msgTxtBox.Text = "";
+            if (msgTxtBox.TextLength > 255)
+            {
+                MessageBox.Show("Message cannot be longer than 255 characters!");
+            }
+            else
+            {
+                Task.Run(async () =>
+                {
+                    return await DataBase.sendMessage(text, this.userID, viewID.ToString());
+                }).ContinueWith(async (reult) =>
+                {
+                    if (reult.Result)
+                    {
+                        populateData();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to send message");
+                    }
+                });
+            }
+        }
+
+        private void msgTxtBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Return)
+            {
+                sendBtn_Click(this, EventArgs.Empty);
+            }
+        }
+
+        virtual protected void multiMsgBtn_Click(object sender, EventArgs e)
+        {
+            MultiMessage m = new MultiMessage(this.userID);
+            m.Show();
+            this.Hide();
+        }
+
+        protected virtual void MessageForm_Load(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                await DataBase.setRead(this.userID, this.viewID);
+            }).ContinueWith((_) => { populateData(); });
+        }
+
+        protected void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            charLbl.Text = msgTxtBox.TextLength.ToString() + " / 255";
+            if(msgTxtBox.TextLength > 255)
+            {
+                msgTxtBox.BackColor = Color.FromArgb(255, 183, 183);
+                charLbl.ForeColor = Color.FromArgb(186, 49, 49);
+            }
+            else
+            {
+                msgTxtBox.BackColor = SystemColors.ControlLightLight;
+                charLbl.ForeColor = SystemColors.ControlDarkDark;
+            }
+        }
+    }
+}
---
+++ Forms/Message Forms/UnreadMessages.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacebookUI.Forms.Message_Forms
+{
+    class UnreadMessages : MessageForm
+    {
+        private new int userID;
+        public UnreadMessages(int userID) : base()
+        {
+            this.userID = userID;
+            base.toTxtBox.Visible = false;
+            base.msgLbl.Text = "Unread Messages";
+            base.msgTxtBox.Visible = false;
+            base.charLbl.Visible = false;
+            base.sendBtn.Text = "View Conversation";
+            base.multiMsgBtn.Visible = false;
+        }
+        private void loadData()
+        {
+            Task.Run(async () =>
+            {
+                return await DataBase.getUnreadMessages(this.userID);
+            }).ContinueWith((result) =>
+            {
+                Util.SetControlPropertyThreadSafe(base.messageDGV, "DataSource", result.Result);
+            });
+        }
+        //Have to do this to over ride the base trying to load data
+        protected override void MessageForm_Load(object sender, EventArgs e)
+        {
+            this.loadData();
+        }
+        protected override void sendBtn_Click(object sender, EventArgs e)
+        {
+            string viewName = base.messageDGV.Rows[base.messageDGV.CurrentCell.RowIndex].Cells[0].Value.ToString();
+            int viewID = int.Parse(base.messageDGV.Rows[base.messageDGV.CurrentCell.RowIndex].Cells[2].Value.ToString());
+            MessageForm mf = new MessageForm(this.userID, viewID, viewName);
+            mf.Show();
+            this.Hide();
+
+
+        }
+    }
+}
