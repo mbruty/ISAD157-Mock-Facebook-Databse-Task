8d6ed4c74ef3a7e637479d0c3aa4a1e6e4efca01
mbruty <48282784+BigBoiBruty@users.noreply.github.com>
2020-05-03 23:00:01 +0100

    Updated to allow for inheritance

---
+++ Forms/Friend Forms/FriendsForm.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookUI
+{
+    public partial class FriendsForm : Form
+    {
+        private int userID;
+        public EventHandler viewBtnClick;
+        public FriendsForm()
+        {
+            InitializeComponent();
+        }
+        public FriendsForm(int userID, bool ownProfile)
+        {
+            InitializeComponent();
+            this.userID = userID;
+            populateData();
+            if (!ownProfile)
+            {
+                friendsBtn.Hide();
+                msgBtn.Hide();
+            }
+        }
+
+        public int getSelectedProfile()
+        {
+            return int.Parse(friendsDGV.Rows[friendsDGV.CurrentCell.RowIndex].Cells[2].Value.ToString());
+        }
+
+        protected virtual void populateData()
+        {
+            Task.Run(async () =>
+            {
+                DataTable result = await DataBase.getUserFriends(this.userID);
+                Util.SetControlPropertyThreadSafe(friendsDGV, "DataSource", result);
+            });
+        }
+
+        private void friendsBtn_Click(object sender, EventArgs e)
+        {
+            int friendID = int.Parse(friendsDGV.Rows[friendsDGV.CurrentCell.RowIndex].Cells[2].Value.ToString());
+            Task.Run(async () =>
+            {
+                bool result = await DataBase.removeFriend(this.userID, friendID);
+                if (result)
+                {
+                    MessageBox.Show("Removed Friend");
+                    populateData();
+                }
+                else
+                {
+                    MessageBox.Show("Failed to remove friend");
+                }
+            });
+        }
+
+        protected virtual void viewProfileBtn_Click(object sender, EventArgs e)
+        {
+            viewBtnClick(sender, e);
+        }
+
+        protected virtual void msgBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
